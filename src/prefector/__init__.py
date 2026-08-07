3a57735ffcc294ca6408d6919a3307326cd01311
Jacob <jacobdicken2005@gmail.com>
2026-08-07 12:30:50 +0100

    Update re-export of block sources

--- src/prefector/__init__.py
+++ src/prefector/__init__.py
@@ -1,6 +1,7 @@
 """Reusable Prefect deployment helpers."""
 
 from prefector.blocks.base import BlockSpec as BlockSpec
-from prefector.blocks.base import settings_model_for_block as settings_model_for_block
+from prefector.blocks.sources.env import env_settings_model_for_block as env_settings_model_for_block
+from prefector.blocks.sources.keeper import keeper_settings_model_for_block as keeper_settings_model_for_block
 from prefector.deployments.base import DeploymentSpec as DeploymentSpec
 from prefector.deployments.base import NonEmptyStr as NonEmptyStr
